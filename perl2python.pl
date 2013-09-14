8fae68e573d6bb4fa60328c8be08d0fcf279434d
Charbel Zeaiter <z3419481@student.unsw.edu.au>
2013-09-14 22:00:02 +1000

    Converted sample code & added print function bracket compatibility.

--- perl2python.pl
+++ perl2python.pl
@@ -1,29 +1,49 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
-# written by andrewt@cse.unsw.edu.au September 2013
-# as a starting point for COMP2041/9041 assignment 
-# http://cgi.cse.unsw.edu.au/~cs2041/13s2/assignments/perl2python
+###############################################################################
+#  File Name:                       perl2python.pl
+# 
+#  Author:                          Charbel Zeaiter
+#
+#  CSE username:                    cjze477
+# 
+#  Student Email:                   z3419481@student.unsw.edu.au
+#
+#  Date Created:                    14/09/2013
+#
+#  Purpose:                         Assignment 1 >> Perl to Python Compiler.
+###############################################################################
 
-while ($line = <>) {
-	if ($line =~ /^#!/ && $. == 1) {
-	
-		# translate #! line 
-		
+# Behave like unix filter and read in lines.
+while ($line = <>)
+{
+   # Start matching and translating.
+   if ($line =~ /^#!/ && $. == 1) {
+      	
+		# Translate Shebang '#!' line. 
 		print "#!/usr/bin/python2.7 -u\n";
+		
 	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
 	
-		# Blank & comment lines can be passed unchanged
-		
+		# Blank lines & comment lines can be passed though unchanged.
 		print $line;
-	} elsif ($line =~ /^\s*print\s*"(.*)\\n"[\s;]*$/) {
+		
+	} elsif ($line =~ /^\s*print\s*\(*"(.*)\\n"\)*[\s;]*$/) {
+
 		# Python's print adds a new-line character by default
 		# so we need to delete it from the Perl print statement
+		print "print \"".$1."\"\n";
 		
-		print "print '$1'\n";
 	} else {
 	
-		# Lines we can't translate are turned into comments
+		# Lines that can't be translated are turned into comments.
+		print "# Could not translate: \"".$line."\"\n";
 		
-		print "#$line\n";
 	}
 }
+
+
+
+
+
+
